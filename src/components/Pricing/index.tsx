07320d72aca77e4b6e0862f9ee1315cf45770b0f
Andrei Canta <deiucanta@gmail.com>
2022-04-23 00:16:03 +0200

    pricing: developer edition and business edition

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -15,69 +15,46 @@ export default function Pricing(): JSX.Element {
           If you are a developer, you can use Easypanel for free.
         </p>
         <div className={styles.plans}>
-          <div className={clsx(styles.plan, styles.planHighlight)}>
-            <h2 className={styles.planName}>Free Forever</h2>
+          <div className={clsx(styles.plan, styles.planDeveloper)}>
+            <h2 className={styles.planName}>Developer Edition</h2>
             <div className={styles.planPrice}>
               $0 <span>/month</span>
             </div>
             <ul>
-              <li>Unlimited Servers</li>
+              <li>Free Forever</li>
               <li>Single User</li>
               <li>Unlimited Projects</li>
               <li>Unlimited Services</li>
-              <li>Free SSL Certificates</li>
               <li>Community Support</li>
             </ul>
             <Link className="button button--block button--primary" href="/docs">
               Get Started
             </Link>
           </div>
-          <div className={styles.plan}>
-            <h2 className={styles.planName}>Hobby</h2>
+          <div className={clsx(styles.plan, styles.planBusiness)}>
+            <h2 className={styles.planName}>Business Edition</h2>
             <div className={styles.planPrice}>
-              $15 <span>/month</span>
+              $12 <span>/month</span>
             </div>
             <ul>
-              <li>2 Servers</li>
-              <li>Unlimited Users</li>
+              <li>Paid per Server</li>
+              <li>
+                Multiple Users <span>soon</span>
+              </li>
               <li>Unlimited Projects</li>
               <li>Unlimited Services</li>
-              <li>Free SSL Certificates</li>
               <li>Priority Support</li>
-            </ul>
-            <Link className="button button--block button--primary" href="/docs">
-              Get Started
-            </Link>
-          </div>
-          <div className={styles.plan}>
-            <h2 className={styles.planName}>Growth</h2>
-            <div className={styles.planPrice}>
-              $30 <span>/month</span>
-            </div>
-            <ul>
-              <li>5 Servers</li>
-              <li>Unlimited Users</li>
-              <li>Unlimited Projects</li>
-              <li>Unlimited Services</li>
-              <li>Free SSL Certificates</li>
-              <li>Priority Support</li>
-            </ul>
-            <Link className="button button--block button--primary" href="/docs">
-              Get Started
-            </Link>
-          </div>
-          <div className={styles.plan}>
-            <h2 className={styles.planName}>Business</h2>
-            <div className={styles.planPrice}>
-              $60 <span>/month</span>
-            </div>
-            <ul>
-              <li>15 Servers</li>
-              <li>Unlimited Users</li>
-              <li>Unlimited Projects</li>
-              <li>Unlimited Services</li>
-              <li>Free SSL Certificates</li>
-              <li>Priority Support</li>
+              <li>
+                Resource Monitoring <span>soon</span>
+              </li>
+              <li>
+                Role-Based Permissions <span>soon</span>
+              </li>
+              <li>
+                Premium Templates <span>soon</span>
+              </li>
+              <li>Installation Support</li>
+              <li>Migration Support</li>
             </ul>
             <Link className="button button--block button--primary" href="/docs">
               Get Started
