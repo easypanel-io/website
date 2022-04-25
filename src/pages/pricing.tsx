4e64bce9ff901967ccdcbece511ab6b4135b7bd4
Andrei Canta <deiucanta@gmail.com>
2022-04-25 16:15:29 +0200

    fix title for pricing page

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -7,7 +7,7 @@ export default function Page(): JSX.Element {
   return (
     <Layout description="Manage your server without fighting the terminal. Server control panel based on Docker.">
       <Head>
-        <title>Easypanel - Server Control Panel</title>
+        <title>Easypanel Pricing</title>
       </Head>
       <Pricing />
     </Layout>
