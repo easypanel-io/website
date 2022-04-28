236a399d135b1f461cc78ab6bbdb15c740163e34
Andrei Canta <deiucanta@gmail.com>
2022-04-28 14:30:19 +0200

    docs & seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout description="Manage your server without fighting the terminal. Server control panel based on Docker.">
       <Head>
-        <title>Easypanel - Server Control Panel</title>
+        <title>Easypanel - Modern Server Control Panel</title>
       </Head>
       <Hero />
       <Languages />
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,14 +1,13 @@
-import Head from "@docusaurus/Head";
 import Pricing from "@site/src/components/Pricing";
 import Layout from "@theme/Layout";
 import React from "react";
 
 export default function Page(): JSX.Element {
   return (
-    <Layout description="Manage your server without fighting the terminal. Server control panel based on Docker.">
-      <Head>
-        <title>Easypanel Pricing</title>
-      </Head>
+    <Layout
+      title="Pricing"
+      description="Easypanel comes in two editons: developer edition and business edition. The developer edition will always be free."
+    >
       <Pricing />
     </Layout>
   );
