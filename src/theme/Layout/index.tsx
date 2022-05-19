9fd348c5f16b632fae0de3e06ed1de8d7d911daa
Andrei Canta <deiucanta@gmail.com>
2022-05-19 15:46:08 +0200

    add clarity script

---
+++ src/theme/Layout/index.tsx
@@ -0,0 +1,20 @@
+import Head from "@docusaurus/Head";
+import Layout from "@theme-original/Layout";
+import React from "react";
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Head>
+        <script type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "byvgkgmvh3");`}
+        </script>
+      </Head>
+      <Layout {...props} />
+    </>
+  );
+}
