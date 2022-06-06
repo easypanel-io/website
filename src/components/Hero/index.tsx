45db0a8bfd4040210e034323a9db1be0ffde6852
Andrei Canta <deiucanta@gmail.com>
2022-06-06 21:50:43 +0200

    add partner logos

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,67 +1,37 @@
 import Link from "@docusaurus/Link";
+import PlayButtonSVG from "@site/static/img/play-button.svg";
 import clsx from "clsx";
 import React from "react";
 import styles from "./styles.module.css";
 
-const playIcon = (
-  <svg
-    width="30"
-    height="30"
-    viewBox="0 0 64 64"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g filter="url(#filter0_d_602_846)">
-      <circle cx="32" cy="30" r="30" fill="#059669" />
-    </g>
-    <path
-      d="M42.9687 30.2175L26.842 39.5283L26.842 20.9067L42.9687 30.2175Z"
-      fill="white"
-    />
-    <defs>
-      <filter
-        id="filter0_d_602_846"
-        x="0.325774"
-        y="0"
-        width="63.3485"
-        height="63.3485"
-        filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
-      >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
-        <feColorMatrix
-          in="SourceAlpha"
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset dy="1.67423" />
-        <feGaussianBlur stdDeviation="0.837113" />
-        <feComposite in2="hardAlpha" operator="out" />
-        <feColorMatrix
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-        />
-        <feBlend
-          mode="normal"
-          in2="BackgroundImageFix"
-          result="effect1_dropShadow_602_846"
-        />
-        <feBlend
-          mode="normal"
-          in="SourceGraphic"
-          in2="effect1_dropShadow_602_846"
-          result="shape"
-        />
-      </filter>
-    </defs>
-  </svg>
-);
+type Partner = {
+  href: string;
+  logo: string;
+};
+
+const partners: Partner[] = [
+  {
+    href: "https://aws.amazon.com/marketplace/pp/prodview-nkiq4lewjosjc",
+    logo: "/partners/aws.svg",
+  },
+  {
+    href: "https://marketplace.digitalocean.com/apps/easypanel",
+    logo: "/partners/digitalocean.svg",
+  },
+  {
+    href: "https://www.linode.com/marketplace/apps/easypanel/easypanel/",
+    logo: "/partners/linode.svg",
+  },
+  {
+    href: "https://www.vultr.com/marketplace/apps/easypanel",
+    logo: "/partners/vultr.svg",
+  },
+];
 
 export default function Hero(): JSX.Element {
   return (
     <>
-      <section className="tw-bg-black tw-py-20 tw-px-3 tw-text-center">
+      <section className="tw-bg-black tw-py-24 tw-px-3 tw-text-center">
         <div className="tw-text-sm lg:tw-text-base tw-text-emerald-600 tw-uppercase tw-font-bold tw-tracking-wider tw-mb-4">
           Powered by Docker
         </div>
@@ -75,20 +45,38 @@ export default function Hero(): JSX.Element {
             target="_blank"
             className="tw-inline-flex tw-items-center"
           >
-            {playIcon}{" "}
+            <PlayButtonSVG className="tw-h-8" />
             <span className="tw-text-emerald-100 tw-ml-3">
               Watch Demo Video
             </span>
           </Link>
         </div>
         <input
-          className="tw-bg-neutral-800 tw-p-4 tw-border-0 tw-rounded-lg tw-font-mono tw-font-bold tw-text-sm tw-text-center tw-w-[430px] tw-max-w-full"
-          value="curl -sSL https://easypanel.io/install.sh | sh"
+          className="tw-bg-neutral-800 tw-p-4 tw-border-0 tw-rounded-lg tw-font-mono tw-font-bold tw-text-sm tw-text-center tw-w-[430px] tw-max-w-full tw-cursor-pointer"
+          defaultValue="curl -sSL https://easypanel.io/install.sh | sh"
           onFocus={(e) => e.currentTarget.select()}
           onSelect={(e) => e.currentTarget.select()}
+          // onChange={() =>{}}
+          readOnly
         />
         <div className="tw-text-neutral-400 tw-mt-2">
           Run this command on a fresh linux server
+        </div>
+        {/* <div className="tw-mt-20 tw-flex tw-flex-col lg:tw-flex-row tw-justify-between tw-gap-6 tw-max-w-4xl tw-mx-auto"> */}
+        <div className="tw-mt-20 tw-flex tw-overflow-x-auto tw-p-4 tw-justify-between tw-gap-10 tw-max-w-4xl tw-mx-auto">
+          {partners.map((partner, index) => (
+            <Link
+              key={index}
+              href={partner.href}
+              target="_blank"
+              className="tw-opacity-30 hover:tw-opacity-100 tw-transition"
+            >
+              <img
+                src={partner.logo}
+                className="tw-h-7 tw-max-w-none lg:tw-h-10 tw-inline-block"
+              />
+            </Link>
+          ))}
         </div>
       </section>
       <section className={styles.root}>
