d813567c269fde114df825ffa73378e7dc62ede9
serban-alexandru <alexserb2009@yahoo.com>
2023-09-04 13:07:25 +0300

    templates search

--- src/pages/templates.tsx
+++ src/pages/templates.tsx
@@ -1,9 +1,27 @@
 import Link from "@docusaurus/Link";
 import templates from "@site/docs/04-templates/templates.json";
 import Layout from "@theme/Layout";
-import React from "react";
+import Fuse from "fuse.js";
+import React, { useMemo, useState } from "react";
 
 export default function Templates(): JSX.Element {
+  const [query, setQuery] = useState("");
+
+  const fuse = useMemo(
+    () =>
+      new Fuse(templates, {
+        keys: ["name", "description", "tags"],
+        threshold: 0.2,
+        ignoreLocation: false,
+      }),
+    []
+  );
+
+  const result = useMemo(
+    () => (query ? fuse.search(query).map((r) => r.item) : templates),
+    [query, fuse]
+  );
+
   return (
     <Layout
       title="Templates"
@@ -18,12 +36,20 @@ export default function Templates(): JSX.Element {
             1-Click Installers for Your Favorite Applications
           </p>
         </div>
+        <div className="tw-flex tw-justify-center tw-items-center tw-mb-4">
+          <input
+            className="tw-py-4 tw-px-6 tw-rounded-xl tw-border-0 tw-text-lg tw-bg-transparent focus:tw-bg-gray-300/10 hover:tw-bg-gray-300/10 tw-w-full md:tw-w-[700px] tw-mt-10 tw-ring-1 tw-ring-inset tw-ring-gray-700 focus:tw-ring-2 focus:tw-ring-emerald-700 tw-outline-none tw-transition "
+            placeholder="Search..."
+            value={query}
+            autoFocus
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
         <div className="tw-max-w-7xl tw-mx-auto tw-mt-16 tw-grid tw-grid-cols-2 lg:tw-grid-cols-4 tw-gap-4 lg:tw-gap-8">
-          {templates.map((template) => (
+          {result.map((template) => (
             <Link
               key={template.slug}
               href={`/docs/templates/${template.slug}`}
-              // className="tw-pt-10 tw-pb-6 tw-bg-white tw-rounded-lg tw-text-gray-500 hover:tw-text-gray-900 hover:tw-scale-110 tw-transition-all"
               className="tw-p-6 tw-bg-gray-800 tw-rounded-lg tw-shadow-highlight hover:tw-scale-105 tw-transition-all"
             >
               {template.logo ? (
